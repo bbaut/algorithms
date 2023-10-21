5dd1ececfce0fa9db93e0259f42515b5754518b4
BranBautista <bbrandon.abo@gmail.com>
2023-10-21 00:28:36 -0600

    first commit

--- Search-Sort/exercise40.js
+++ Search-Sort/exercise40.js
@@ -1,1 +1,55 @@
 // Find the "2nd" largest number in a stream
+
+function randomNumbers() {
+    function _stream(top = 1000, bottom = 1) {
+        return {
+            value: Math.floor( Math.random() * ( 1 + top - bottom ) ) + bottom,
+            next() {
+            return _stream(top = 1000, bottom = 1);
+            }
+        };
+    }
+   
+    return () => _stream(top = 1000, bottom = 1);
+}
+   
+const randoms = randomNumbers();
+  
+function take(n, str) {
+    function _take(n, str, accum) {
+        if (n === 0) {
+            return accum;
+        }
+    
+        const { value, next } = str();
+    
+        return _take(n - 1, next, accum.concat(value));
+    }
+   
+    return _take(n, str, []);
+}
+
+const oneHundredRandom = take(100, randoms);
+
+function largestNum(array, kthNumber) {
+    if(kthNumber < 1 || kthNumber > array.length) {
+        return null;
+    }
+    let sortedArray = bubbleSort(array);
+    return sortedArray[array.length - kthNumber];
+}
+
+function bubbleSort (array) {
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < array.length; j++){
+            if(array[j] > array[j+1]){
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+console.log(largestNum(oneHundredRandom, 2));
--- Trees/exercise56.js
+++ Trees/exercise56.js
@@ -1,1 +1,45 @@
 // Given an input stream, detect when the series of characters correspond to a word found in a word array
+
+function makeid(length) {
+    let result = '';
+    const characters = 'abcdefghijklmnopqrstuvwxyz';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+function randomNumbers() {
+    function _stream(n) {
+        return {
+            value: makeid(n),
+            next() {
+            return _stream(n);
+            }
+        };
+    }
+   
+    return () => _stream(1);
+}
+
+const randoms = randomNumbers();
+
+function take(n, str) {
+    function _take(n, str, accum) {
+        if (n === 0) {
+            return accum;
+        }
+
+        const { value, next } = str();
+    
+        return _take(n - 1, next, accum.concat(value));
+    }
+   
+    return _take(n, str, []);
+}
+
+const oneHundredRandom = take(3, randoms);
+console.log(oneHundredRandom)
